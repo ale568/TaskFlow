8b1fdccdb6e36e421edb41fb18063139eaefe9bf
Alessandro Cofone <alessandro.cofone@yahoo.com>
2025-02-02 16:35:11 +0100

    [REFACTOR] - ActivityClass - End Refactoring - #002 - 2025-02-02 16:35:11 		    Details: 		    - Time Taken: 3 min 		    - Implemented Methods: constuctor() 		    - Number of Test-Fail-Pass Interations: 0 		    - Number of Bugs Fixed: 0 		    - Number of Refactorings: 1

--- renderer/models/activity.js
+++ renderer/models/activity.js
@@ -1,9 +1,22 @@
 class Activity {
-    constructor(name, duration, project = null) {
+    constructor(name, duration, project = null, startTime = new Date()) {
+        if (typeof name !== 'string' || name.trim() === '') {
+            throw new Error('Invalid name');
+        }
+        if (typeof duration !== 'number' || duration <= 0) {
+            throw new Error('Invalid duration');
+        }
+        if (project !== null && typeof project !== 'string') {
+            throw new Error('Invalid project');
+        }
+        if (!(startTime instanceof Date) || isNaN(startTime.getTime())) {
+            throw new Error('Invalid startTime');
+        }
+
         this.name = name;
         this.duration = duration;
         this.project = project;
-        this.startTime = new Date();
+        this.startTime = startTime;
         this.endTime = new Date(this.startTime.getTime() + duration * 1000);
     }
 }
