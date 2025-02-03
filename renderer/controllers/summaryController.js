50c1bc684a60d3bb1cde32ad457a4edba49eb357
Alessandro Cofone <alessandro.cofone@yahoo.com>
2025-02-03 17:29:44 +0100

    [REFACTOR] - SummaryControllerClass - End Refactoring - #011 - 2025-02-03 17:29:44 Details: - Time Taken: 2 min - Implemented Methods: getTagUsageSummary() - Number of Test-Fail-Pass Interations: 0 - Number of Bugs Fixed: 0 - Number of Refactorings: 1

--- renderer/controllers/summaryController.js
+++ renderer/controllers/summaryController.js
@@ -1,36 +1,43 @@
 class SummaryController {
+    getSummary(projects) {
+        let totalWorkedHours = 0;
+        const activitySummaryByProject = {};
+        const tagSummary = new Map();
+
+        projects.forEach(project => {
+            let projectTotal = 0;
+            project.activities.forEach(activity => {
+                totalWorkedHours += activity.duration;
+                projectTotal += activity.duration;
+
+                const tag = activity.tag;
+                if (tag) {
+                    if (!tagSummary.has(tag.name)) {
+                        tagSummary.set(tag.name, 0);
+                    }
+                    tagSummary.set(tag.name, tagSummary.get(tag.name) + 1);
+                }
+            });
+            activitySummaryByProject[project.name] = projectTotal;
+        });
+
+        return {
+            totalWorkedHours,
+            activitySummaryByProject,
+            tagSummary: Object.fromEntries(tagSummary)
+        };
+    }
+
     getTotalWorkedHours(projects) {
-        return projects.reduce((total, project) => {
-            return total + project.activities.reduce((projectTotal, activity) => {
-                return projectTotal + activity.duration;
-            }, 0);
-        }, 0);
+        return this.getSummary(projects).totalWorkedHours;
     }
 
     getActivitySummaryByProject(projects) {
-        const summary = {};
-        projects.forEach(project => {
-            summary[project.name] = project.activities.reduce((total, activity) => {
-                return total + activity.duration;
-            }, 0);
-        });
-        return summary;
+        return this.getSummary(projects).activitySummaryByProject;
     }
 
     getTagUsageSummary(projects) {
-        const tagSummary = {};
-        projects.forEach(project => {
-            project.activities.forEach(activity => {
-                const tag = activity.tag;
-                if (tag) {
-                    if (!tagSummary[tag.name]) {
-                        tagSummary[tag.name] = 0;
-                    }
-                    tagSummary[tag.name]++;
-                }
-            });
-        });
-        return tagSummary;
+        return this.getSummary(projects).tagSummary;
     }
 }
 
