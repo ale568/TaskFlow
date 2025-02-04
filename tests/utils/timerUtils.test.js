7e108d07ababc48597b2a11019bdd9b639162957
Alessandro Cofone <alessandro.cofone@yahoo.com>
2025-02-04 18:28:40 +0100

    [REFACTOR] - TimerUtilsClass - End Refactoring - #013 - 2025-02-04 18:28:40 Details: - Time Taken: 2 min - Implemented Methods: resetSettings(), getTotalTime(intervals), reset() - Number of Test-Fail-Pass Interations: 0 - Number of Bugs Fixed: 0 - Number of Refactorings: 1

--- tests/utils/timerUtils.test.js
+++ tests/utils/timerUtils.test.js
@@ -24,4 +24,36 @@ describe('Timer Utils', () => {
         timer.pause();
         expect(timer.isRunning).toBe(false);
     });
+
+    test('formatTime should manage invalid values', () => {     // edge cases
+        expect(timerUtils.formatTime(-10)).toBe('00:00:00');
+        expect(timerUtils.formatTime(null)).toBe('00:00:00');
+        expect(timerUtils.formatTime(undefined)).toBe('00:00:00');
+    });
+
+    test('getTotalTime should manage incomplete intervals', () => {
+        const intervals = [
+            {start: 0, end: 60}, // 1 min
+            {start: 120, }, 
+            {end: 300},     
+            {start: 600, end: 1200}     // 10 min
+        ];
+        expect(timerUtils.getTotalTime(intervals)).toBe(11 * 60);   // Only the two valid intervals count 
+    });
+
+    test('Timer reset() should reset the elapsed time to zero', () => {
+        const timer = new timerUtils.Timer();
+        timer.start();
+        timer.pause();
+        timer.reset();
+        expect(timer.elapsedTime).toBe(0);
+    });
+
+    test('The method start() should not overwrite startTime if already running', () => {
+        const timer = new timerUtils.Timer();
+        timer.start();
+        const firstStartTime = timer.startTime;
+        timer.start();
+        expect(timer.startTime).toBe(firstStartTime);
+    });
 });
