d6f633d49b881d9452456b2b36d809ea55a015b5
Alessandro Cofone <alessandro.cofone@yahoo.com>
2025-02-05 09:16:46 +0100

    [DEV] - TimerUtilsClass - Start Implementation - #014 - 2025-02-05 09:16:46

---
+++ tests/utils/dateUtils.test.js
@@ -0,0 +1,27 @@
+const dateUtils = require('../../renderer/utils/dateUtils');
+
+describe('Date Utils', () => {
+
+    test('It should format the date correctly', () => {
+        const date = new Date('2025-01-10T15:40:45');
+        expect(dateUtils.formatDate(date, 'YYYY-MM-DD')).toBe('2025-01-10');
+        expect(dateUtils).formatDate(date, 'DD/MM/YYYY').toBe('10/01/2025');
+    });
+
+    test('It should calculate the difference between two dates in days', () => {
+        const start = new Date('2025-02-04');
+        const end = new Date('2025-01-30');
+        expect(dateUtils.dateInDays(start, end)).toBe(5);
+    });
+
+    test('It should verify if a date is in the future', () => {
+        const futureDate = new Date(Date.now() + 10000000);
+        expect(dateUtils.isFutureDate(futureDate)).toBe(true);
+    });
+
+    test('It should add and subtract days from a date', () => {
+        const initialDate = new Date('2025-01-15');
+        expect(dateUtils.addDays(initialDate, 5).toISOString().slice(0, 10)).toBe('2025-01-20');
+        expect(dateUtils.addDays(initialDate, -4).toISOString().slice(0, 10)).toBe('2025-01-11');
+    });
+});
