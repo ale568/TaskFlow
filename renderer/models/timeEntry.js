56463760867dc96f1297fbb01ad6fd3180cadce7
Alessandro Cofone <alessandro.cofone@yahoo.com>
2025-02-12 16:09:04 +0100

    [REFACTOR] - TimeEntryClass - End Refactoring - #030 - 2025-02-12 16:09:04 Details: - Time Taken: 3 min - Implemented Methods: isValid(), toJSON() - Number of Test-Fail-Pass Interations: 0 - Number of Bugs Fixed: 0 - Number of Refactorings: 1

--- renderer/models/timeEntry.js
+++ renderer/models/timeEntry.js
@@ -11,6 +11,20 @@ class TimeEntry {
         Object.assign(this, fields);
     }
 
+    isValid() {
+        return this.project && this.task && this.startTime && this.duration;
+    }
+
+    toJSON() {
+        return {
+            id: this.id,
+            project: this.project,
+            task: this.task,
+            startTime: this.startTime,
+            duration: this.duration
+        };
+    }
+
     static createFromDbRow(row) {
         return new TimeEntry(row.id, row.project, row.task, row.startTime, row.duration);
     }
