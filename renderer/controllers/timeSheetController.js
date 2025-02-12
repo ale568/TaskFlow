7270aa257a986ff8452bd6f31f171111fecae341
Alessandro Cofone <alessandro.cofone@yahoo.com>
2025-02-12 15:44:46 +0100

    [DEV] - TimeSheetControllerClass - End Implementation - #029 - 2025-02-12 15:44:46 Details: - Time Taken: 2 min - Implemented Methods: allMethods() - Number of Test-Fail-Pass Iterations: 0 - Number of Bugs Fixed: 0 - Number of Refactorings: 0

--- renderer/controllers/timeSheetController.js
+++ renderer/controllers/timeSheetController.js
@@ -1,32 +1,63 @@
+const dbUtils = require('../utils/dbUtils');
 const TimeEntry = require('../models/timeEntry');
 
 class TimeSheetController {
     constructor() {
-        this.entries = [];
+        dbUtils.connect();
     }
 
-    addEntry(entry) {
-        this.entries.push(entry);
+    async addEntry(entry) {
+        const query = `
+            INSERT INTO time_entries (project, task, startTime, duration)
+            VALUES (?, ?, ?, ?)
+        `;
+        const params = [entry.project, entry.task, entry.startTime, entry.duration];
+        await dbUtils.runQuery(query, params);
+
+        const lastIdQuery = `SELECT last_insert_rowid() AS id`;
+        const lastIdResult = await dbUtils.runQuery(lastIdQuery);
+
+        return new TimeEntry(
+            lastIdResult[0].id,
+            entry.project,
+            entry.task,
+            entry.startTime,
+            entry.duration
+        );
     }
 
-    removeEntry(id) {
-        this.entries = this.entries.filter(entry => entry.id !== id);
+    async removeEntry(id) {
+        const query = `DELETE FROM time_entries WHERE id = ?`;
+        await dbUtils.runQuery(query, [id]);
     }
 
-    getEntries() {
-        return this.entries;
+    async getEntries() {
+        const query = `SELECT * FROM time_entries`;
+        const rows = await dbUtils.runQuery(query);
+
+        return rows.map(row => TimeEntry.createFromDbRow(row));
     }
 
-    getEntriesByProject(project) {
-        return this.entries.filter(entry => entry.project === project);
+    async getEntriesByProject(project) {
+        const query = `SELECT * FROM time_entries WHERE project = ?`;
+        const rows = await dbUtils.runQuery(query, [project]);
+
+        return rows.map(row => TimeEntry.createFromDbRow(row));
     }
 
-    getTotalWorkedHours() {
-        return this.entries.reduce((total, entry) => total + entry.duration, 0);
+    async getTotalWorkedHours() {
+        const query = `SELECT SUM(duration) as total FROM time_entries`;
+        const result = await dbUtils.runQuery(query);
+
+        return result[0]?.total || 0;
     }
 
-    setEntries(entries) {
-        this.entries = entries;
+    async setEntries(entries) {
+        await dbUtils.runQuery(`DELETE FROM time_entries`);
+
+        for (const entry of entries) {
+            await this.addEntry(entry);
+        }
     }
 }
 
