bf409791746107f8bce5c6937e15da185540959d
Alessandro Cofone <alessandro.cofone@yahoo.com>
2025-02-13 11:27:14 +0100

    [REFACTOR] - TimeEntryClass - End Refactoring - #030 - 2025-02-13 11:27:14 Details: - Time Taken: 11 min - Implemented Methods: - Number of Test-Fail-Pass Interations: 2 - Number of Bugs Fixed: 0 - Number of Refactorings: 2

--- renderer/models/timeEntry.js
+++ renderer/models/timeEntry.js
@@ -1,10 +1,10 @@
 class TimeEntry {
     constructor(id, project, task, startTime, duration) {
-        this.id = id;
-        this.project = project;
-        this.task = task;
-        this.startTime = startTime;
-        this.duration = duration;
+        this.id = id || null;
+        this.project = project || '';
+        this.task = task !== undefined ? task : '';
+        this.startTime = startTime || '';
+        this.duration = duration || 0;
     }
 
     update(fields) {
@@ -12,20 +12,38 @@ class TimeEntry {
     }
 
     isValid() {
-        return this.project && this.task && this.startTime && this.duration;
+        return (
+            typeof this.project === 'string' && this.project.trim() !== '' &&
+            typeof this.task === 'string' && this.task.trim() !== '' &&
+            typeof this.startTime === 'string' && this.startTime.trim() !== '' &&
+            typeof this.duration === 'number' && this.duration > 0
+        );
     }
 
     toJSON() {
         return {
             id: this.id,
-            project: this.project,
-            task: this.task,
-            startTime: this.startTime,
-            duration: this.duration
+            project: this.project || '',
+            task: this.task !== undefined ? this.task : undefined,
+            startTime: this.startTime || '',
+            duration: this.duration || 0
         };
     }
 
+    clone() {
+        return new TimeEntry(this.id, this.project, this.task, this.startTime, this.duration);
+    }
+
     static createFromDbRow(row) {
+        if (
+            !row.id ||
+            typeof row.project !== 'string' || row.project.trim() === '' ||
+            typeof row.task !== 'string' || row.task.trim() === '' ||
+            typeof row.startTime !== 'string' || row.startTime.trim() === '' ||
+            typeof row.duration !== 'number' || row.duration <= 0
+        ) {
+            throw new Error('Invalid data');
+        }
         return new TimeEntry(row.id, row.project, row.task, row.startTime, row.duration);
     }
 }
