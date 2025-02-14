07f2c680702b29938ade5673eafaeb1168e6daf7
Alessandro Cofone <alessandro.cofone@yahoo.com>
2025-02-14 17:57:17 +0100

    [DEV] - ActivityClass - End Implementation - #035 - 2025-02-14 17:57:17 Details: - Time Taken: 5 min - Implemented Methods: constructor() - Number of Test-Fail-Pass Iterations: 1 - Number of Bugs Fixed: 0 - Number of Refactorings: 0

--- renderer/models/activity.js
+++ renderer/models/activity.js
@@ -1,12 +1,12 @@
 class Activity {
-    constructor(id, name, duration, project_id = null) {
+    constructor(id, name, duration = 0, project_id = null, startTime = new Date()) {
         if (typeof id !== 'number' || id <= 0) {
             throw new Error('Invalid id');
         }
         if (typeof name !== 'string' || name.trim() === '') {
             throw new Error('Invalid name');
         }
-        if (typeof duration !== 'number' || duration <= 0) {
+        if (typeof duration !== 'number' || duration < 0) {
             throw new Error('Invalid duration');
         }
         if (project_id !== null && typeof project_id !== 'number') {
@@ -17,6 +17,8 @@ class Activity {
         this.name = name;
         this.duration = duration;
         this.project_id = project_id;
+        this.startTime = startTime;
+        this.endTime = new Date(this.startTime.getTime() + duration * 1000);
         this.createdAt = new Date();
     }
 
@@ -25,12 +27,14 @@ class Activity {
             id: this.id,
             name: this.name,
             duration: this.duration,
-            project_id: this.project_id
+            project_id: this.project_id,
+            start_time: this.startTime.toISOString(),
+            end_time: this.endTime.toISOString()
         };
     }
 
     static createFromDbRow(row) {
-        return new Activity(row.id, row.name, row.duration, row.project_id);
+        return new Activity(row.id, row.name, row.duration, row.project_id, new Date(row.start_time));
     }
 }
 
