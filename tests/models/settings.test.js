205e15bc08edf9061869bd01d5e2cc8c730e2905
Alessandro Cofone <alessandro.cofone@yahoo.com>
2025-02-15 14:57:53 +0100

    [DEV] - SettingsClass - Start Implementation - #037 - 2025-02-15 14:57:53

--- tests/models/settings.test.js
+++ tests/models/settings.test.js
@@ -1,39 +1,63 @@
 const Settings = require('../../renderer/models/settings');
 
-describe('Settings', () => {
+describe('Settings Model', () => {
 
-    test('It should create a settings object with default values', () => {
-        const settings = new Settings();
+    let settings;
+
+    beforeEach(() => {
+        settings = new Settings();
+    });
+
+    test('it should create an instance with default settings', () => {
         expect(settings.timeFormat).toBe('24h');
         expect(settings.dateFormat).toBe('YYYY-MM-DD');
         expect(settings.language).toBe('en');
         expect(settings.notificationsEnabled).toBe(true);
     });
 
-    test('It should allow to update time\'s format', () => {
-        const settings = new Settings();
+    test('It should update the time format', () => {
         settings.setTimeFormat('12h');
         expect(settings.timeFormat).toBe('12h');
     });
 
-    test('It should allow to update date\'s format', () => {
-        const settings = new Settings();
-        settings.setDateFormat('DD/MM/YYYY');
-        expect(settings.dateFormat).toBe('DD/MM/YYYY');
+    test('It should update the date format', () => {
+        settings.setDataFormat('MM/DD/YYYY');
+        expect(settings.dateFormat).toBe('MM/DD/YYYY');
     });
 
-    test('It should allow to update language', () => {
-        const settings = new Settings();
+    test('It should update the language', () => {
         settings.setLanguage('it');
         expect(settings.language).toBe('it');
     });
 
-    test('It should allow to enable and disable notifications', () => {
-        const settings = new Settings();
+    test('It should anable or disable notifications', () => {
         settings.setNotifications(false);
         expect(settings.notificationsEnabled).toBe(false);
-        settings.setNotifications(true);
-        expect(settings.notificationsEnabled).toBe(true);
-        
+    });
+
+    test('It should convert an instance to a database object', () => {
+        const dbObject = settings.toDbObject();
+        expect(dbObject).toEqual([
+            { key: 'timeFormat', value: '24h' },
+            { key: 'dateFormat', value: 'YYYY-MM-DD' },
+            { key: 'language', value: 'en' },
+            { key: 'notificationsEnabled', value: 'true' },
+        ]);
+    });
+
+    test('It should create an instance from a database row', () => {
+        const dbRows = [
+            { key: 'timeFormat', value: '12h' },
+            { key: 'dateFormat', value: 'DD-MM-YYYY' },
+            { key: 'language', value: 'it' },
+            { key: 'notificationsEnabled', value: 'false' }
+        ];
+
+        const settingsInstance = Settings.createFromDbRows(dbRows);
+
+        expect(settingsInstance.timeFormat).toBe('12h');
+        expect(settingsInstance.dateFormat).toBe('DD-MM-YYYY');
+        expect(settingsInstance.language).toBe('it');
+        expect(settingsInstance.notificationsEnabled).toBe(false);
     });
 });
