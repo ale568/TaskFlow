c31349a883c976370d9ff15f5d6433ef2db19afe
Alessandro Cofone <alessandro.cofone@yahoo.com>
2025-02-16 16:33:38 +0100

    [DEV] - TagsClass - End Implementation - #038 - 2025-02-16 16:33:38 Details: - Time Taken: 2 min - Implemented Methods: allMethods() - Number of Test-Fail-Pass Iterations: 0 - Number of Bugs Fixed: 0 - Number of Refactorings: 0

--- renderer/models/tag.js
+++ renderer/models/tag.js
@@ -1,23 +1,61 @@
+const dbUtils = require('../utils/dbUtils');
+
 class Tag {
     constructor(id, name, color) {
         this.id = id;
-        this.setName(name);
-        this.setColor(color);
-    }
-
-    setName(name) {
-        if (!name) {
-            throw new Error("Tag's name cannot be empty");
-        }
         this.name = name;
+        this.color = color;
     }
 
-    setColor(color) {
-        const hexColorPattern = /^#([0-9A-F]{3}){1,2}$/i;
-        if (!hexColorPattern.test(color)) {
-            throw new Error("Color's format is not valid");
+    // Carica tutti i tag dal database
+    static async loadAllTags() {
+        const query = `SELECT * FROM tags`;
+        const rows = await dbUtils.runQuery(query);
+        return rows.map(row => new Tag(row.id, row.name, row.color));
+    }
+
+    // Crea un nuovo tag nel database
+    static async createTag(name, color) {
+        if (!name || !color) {
+            throw new Error("Invalid tag data");
         }
-        this.color = color;
+
+        const query = `INSERT INTO tags (name, color) VALUES (?, ?)`;
+        const result = await dbUtils.runQuery(query, [name, color]);
+
+        if (result.success) {
+            return new Tag(result.lastInsertRowid, name, color);
+        } else {
+            throw new Error("Failed to insert tag");
+        }
+    }
+
+    // Aggiorna un tag esistente nel database
+    static async updateTag(id, name, color) {
+        if (!id || !name || !color) {
+            throw new Error("Invalid tag update data");
+        }
+
+        const query = `UPDATE tags SET name = ?, color = ? WHERE id = ?`;
+        const result = await dbUtils.runQuery(query, [name, color, id]);
+
+        if (result.success) {
+            return new Tag(id, name, color);
+        } else {
+            throw new Error("Failed to update tag");
+        }
+    }
+
+    // Cancella un tag dal database
+    static async deleteTag(id) {
+        if (!id) {
+            throw new Error("Invalid tag ID");
+        }
+
+        const query = `DELETE FROM tags WHERE id = ?`;
+        const result = await dbUtils.runQuery(query, [id]);
+
+        return result.success;
     }
 }
 
